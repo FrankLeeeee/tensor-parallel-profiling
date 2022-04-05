a7386c8024892f7a5a10f1204f2b97049704aaf6
FrankLeeeee <somerlee.9@gmail.com>
2022-04-05 09:24:18 +0000

    added profiler

--- scripts/run_all_linear.sh
+++ scripts/run_all_linear.sh
@@ -1,23 +1,23 @@
-# bash ./scripts/run.sh 4 configs/gpu4_1d.py dim linear
-# sleep 5
-# bash ./scripts/run.sh 4 configs/gpu4_1d.py bs linear
-# sleep 5
+bash ./scripts/run.sh 4 configs/gpu4_1d.py dim linear
+sleep 5
+bash ./scripts/run.sh 4 configs/gpu4_1d.py bs linear
+sleep 5
 bash ./scripts/run.sh 4 configs/gpu4_2d.py dim linear
 sleep 5
 bash ./scripts/run.sh 4 configs/gpu4_2d.py bs linear
-# sleep 5
-# bash ./scripts/run.sh 4 configs/gpu4_2p5d.py dim linear
-# sleep 5
-# bash ./scripts/run.sh 4 configs/gpu4_2p5d.py bs linear
-# sleep 5
-# bash ./scripts/run.sh 8 configs/gpu8_1d.py dim linear
-# sleep 5
-# bash ./scripts/run.sh 8 configs/gpu8_1d.py bs linear
-# sleep 5
-# bash ./scripts/run.sh 8 configs/gpu8_2p5d.py dim linear
-# sleep 5
-# bash ./scripts/run.sh 8 configs/gpu8_2p5d.py bs linear
-# sleep 5
-# bash ./scripts/run.sh 8 configs/gpu8_3d.py dim linear
-# sleep 5
-# bash ./scripts/run.sh 8 configs/gpu8_3d.py bs linear
+sleep 5
+bash ./scripts/run.sh 4 configs/gpu4_2p5d.py dim linear
+sleep 5
+bash ./scripts/run.sh 4 configs/gpu4_2p5d.py bs linear
+sleep 5
+bash ./scripts/run.sh 8 configs/gpu8_1d.py dim linear
+sleep 5
+bash ./scripts/run.sh 8 configs/gpu8_1d.py bs linear
+sleep 5
+bash ./scripts/run.sh 8 configs/gpu8_2p5d.py dim linear
+sleep 5
+bash ./scripts/run.sh 8 configs/gpu8_2p5d.py bs linear
+sleep 5
+bash ./scripts/run.sh 8 configs/gpu8_3d.py dim linear
+sleep 5
+bash ./scripts/run.sh 8 configs/gpu8_3d.py bs linear
